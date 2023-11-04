8c6ddaf3b1b245e92f04a41b662a9a18c03e3e7d
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-11-04 17:59:19 -0600

    Update settings usage

--- clipboard.py
+++ clipboard.py
@@ -1,15 +1,17 @@
-from talon import clip, Module, actions
+from talon import clip, Module, actions, settings
 
 module = Module()
-clipboard_sleep_setting = module.setting(
-    'google_sheets_clipboard_delay',
+clipboard_sleep_setting_setting_name = 'google_sheets_clipboard_delay'
+clipboard_sleep_setting = 'user.' + clipboard_sleep_setting_setting_name
+module.setting(
+    clipboard_sleep_setting_setting_name,
     type = float,
     default = 0.2,
     desc = 'How long to pause when using the clipboard with google sheets commands. Try increasing this if those commands are not working.'
 )
 
 def wait_long_enough_to_let_clipboard_revert_properly():
-    actions.sleep(clipboard_sleep_setting.get())
+    actions.sleep(settings.get(clipboard_sleep_setting))
 
 def get_selected_text_using_clipboard() -> str:
     with clip.revert():
